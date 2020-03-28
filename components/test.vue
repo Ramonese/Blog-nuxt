1ab844e7321db9f68ee2ab6f5e04734576c3ed66
ieva ozolite <ieva.ozolite@gmail.com>
2020-03-28 19:46:50 +0200

    init commit

---
+++ components/test.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 v-if="true === true" @click="handler(arg, event)">test</h1>
+</template>
+
+<script>
+export default {
+  props: {
+    propName: {
+      type: Number,
+      default: 2
+    }
+  }
+};
+</script>
+
+<style></style>
---
+++ pages/about/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about-page">
+    <h1>About page</h1>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default Vue.extend({});
+</script>
+
+<style scoped>
+.about-page {
+  text-align: center;
+}
+</style>
